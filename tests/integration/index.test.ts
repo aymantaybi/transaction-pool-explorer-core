1ca2c60087c2cb4022ee77f3819f5156274558e7
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2023-08-05 00:39:27 +0100

    Update index.test.ts

--- tests/integration/index.test.ts
+++ tests/integration/index.test.ts
@@ -1,6 +1,8 @@
 import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
 import { Explorer } from "../../src/";
 
+console.log('My Secret Key:', process.env.WEBSOCKET_RPC_ENDPOINT);
+
 const { WEBSOCKET_RPC_ENDPOINT } = process.env;
 
 if (!WEBSOCKET_RPC_ENDPOINT || !WEBSOCKET_RPC_ENDPOINT.startsWith("ws")) throw Error("WEBSOCKET_RPC_ENDPOINT env variable should start with ws");
@@ -11,7 +13,6 @@ const explorer = new Explorer({ url });
 
 describe("Integration Test", () => {
   beforeAll(async () => {
-    console.log('My Secret Key:', process.env.WEBSOCKET_RPC_ENDPOINT);
     await explorer.start();
   });
   afterAll(async () => {
