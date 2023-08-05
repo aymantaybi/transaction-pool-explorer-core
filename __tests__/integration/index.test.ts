ff72fdb24c3c86b363066efd6acecca6c169a7e3
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2023-08-05 03:18:33 +0100

    test: rename tests folder to __tests__ following jest naming conventions

--- __tests__/integration/index.test.ts
+++ __tests__/integration/index.test.ts
@@ -1,5 +1,5 @@
 import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
-import { Explorer } from "../../src/";
+import { Explorer } from "../../src";
 
 const { WEBSOCKET_RPC_ENDPOINT } = process.env;
 
