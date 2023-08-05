197f32d8fb80e17d757a24376a847ab5144b3ac5
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2023-08-05 05:41:57 +0100

    test(integration): add Listen to Replaced Transactions Test

--- __tests__/integration/index.test.ts
+++ __tests__/integration/index.test.ts
@@ -1,13 +1,21 @@
 import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
 import { Explorer } from "../../src";
+import Web3 from "web3";
+import { sendVoidTransaction, waitForEvent } from "../__utils__";
+import { TransactionWithMetadata } from "../../src/interfaces";
 
-const { WEBSOCKET_RPC_ENDPOINT } = process.env;
+const { WEBSOCKET_RPC_ENDPOINT, PRIVATE_KEY } = process.env;
 
-if (!WEBSOCKET_RPC_ENDPOINT || !WEBSOCKET_RPC_ENDPOINT.startsWith("ws")) throw Error("WEBSOCKET_RPC_ENDPOINT env variable should start with ws");
+if (!WEBSOCKET_RPC_ENDPOINT || !WEBSOCKET_RPC_ENDPOINT.startsWith("ws")) throw Error('WEBSOCKET_RPC_ENDPOINT Env Variable Should Start With "ws"');
+if (!PRIVATE_KEY) throw Error("Missing PRIVATE_KEY Env Variable");
 
 const url = WEBSOCKET_RPC_ENDPOINT;
 
-const explorer = new Explorer({ url });
+const web3 = new Web3(new Web3.providers.WebsocketProvider(url));
+
+web3.eth.accounts.wallet.add(PRIVATE_KEY);
+
+const explorer = new Explorer({ web3 });
 
 describe("Integration Test", () => {
   beforeAll(async () => {
@@ -35,5 +43,26 @@ describe("Integration Test", () => {
       }
       done();
     });
+  }, 60000);
+  test("Listen To Replaced Transactions", async () => {
+    const { address } = web3.eth.accounts.wallet[0];
+    const nonce = await web3.eth.getTransactionCount(address);
+    sendVoidTransaction(web3, nonce);
+    const originalTransaction = await waitForEvent<TransactionWithMetadata>(
+      explorer,
+      "transactionAdded",
+      (data) => data.from.toLowerCase() === address.toLowerCase(),
+      10000
+    );
+    sendVoidTransaction(web3, nonce, web3.utils.toWei(5, "Gwei"));
+    const replacementTransaction = await waitForEvent<TransactionWithMetadata>(
+      explorer,
+      "transactionReplaced",
+      (data) => data.from.toLowerCase() === address.toLowerCase(),
+      10000
+    );
+    expect(originalTransaction).toBeDefined();
+    expect(replacementTransaction).toBeDefined();
+    expect(replacementTransaction?.metadata.original).toEqual(originalTransaction);
   }, 60000);
 });
